34755874951a89ef073057b6ae015c027e97ff11
ATrepte2000 <allegra.trepte@unisg.ch>
2024-11-20 18:12:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import openai
 
 
 # Configure page layout
-st.set_page_config(page_title="Empa - Your Reflection Partner", page_icon="💬", layout="centered")
+st.set_page_config(page_title="Your Reflection Partner", page_icon="💬", layout="centered")
 
 
 # Transparent background for the content
